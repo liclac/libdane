5027c37ef54e6dea53727ec8c6f457bc24d90381
uppfinnarn <uppfinnarn@gmail.com>
2015-07-31 14:01:27 +0200

    Argument parsing

--- src/danetool/main.cpp
+++ src/danetool/main.cpp
@@ -12,15 +12,37 @@ using namespace libdane;
 
 #define WORKERS 2
 
+void printUsage(std::string progname = "danetool")
+{
+	std::cerr << "Usage: " << progname << " [domain]" << std::endl;
+	std::cerr << std::endl;
+	std::cerr << "    --verify    Also verify the server's certificate" << std::endl;
+	std::cerr << std::endl;
+}
+
 int main(int argc, char **argv)
 {
 	// C++ify the argument list
 	std::string progname = argv[0];
 	std::vector<std::string> args(argv + 1, argv + argc);
 	
-	// Require an argument (the domain to look up)
-	if (args.size() < 1) {
-		std::cerr << "Usage: " << progname << " [domain]" << std::endl;
+	// Quick, terrible arg parsing
+	bool verify = false;
+	std::string domain;
+	for (auto it = args.begin(); it != args.end(); ++it) {
+		const std::string &s = *it;
+		if (s[0] == '-') {
+			if (s == "--verify") {
+				verify = true;
+			}
+		} else {
+			domain = s;
+		}
+	}
+	
+	// Print usage instructions and quit if no domain is given
+	if (domain.empty()) {
+		printUsage(progname);
 		return 1;
 	}
 	
@@ -32,7 +54,7 @@ int main(int argc, char **argv)
 	DANE dane(service);
 	
 	// Look up the DANE record for the mail server on the domain
-	dane.lookupDANE(std::string("_25._tcp.") + args[0], [&](std::deque<DANERecord> records) {
+	dane.lookupDANE(std::string("_25._tcp.") + domain, [&](std::deque<DANERecord> records) {
 		for (auto it = records.begin(); it != records.end(); ++it) {
 			std::cout << it->toString() << std::endl;
 		}
