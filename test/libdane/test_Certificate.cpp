8ef93ce0c55d5401a1c983a0cddb3b0bc9cfcdb0
uppfinnarn <uppfinnarn@gmail.com>
2015-08-18 13:02:00 +0200

    More tests for Certificate

--- test/libdane/test_Certificate.cpp
+++ test/libdane/test_Certificate.cpp
@@ -10,16 +10,38 @@ SCENARIO("Accessors work")
 	{
 		Certificate cert(resources::googlePEM);
 		
+		THEN("The certificate should be truthy")
+		{
+			CHECK(!!cert);
+		}
+		
 		THEN("Issuer and Subject should be correct")
 		{
 			CHECK(cert.issuerDN() == "/C=US/O=Google Inc/CN=Google Internet Authority G2");
 			CHECK(cert.subjectDN() == "/C=US/ST=California/L=Mountain View/O=Google Inc/CN=*.google.com");
+		}
+		
+		THEN("Data blobs should be correct")
+		{
+			CHECK(cert.encoded().sha256().hex() == "440e875366ad14f997127cb7199dd255fc01fb1c57a7adde9415dca228535404");
+			CHECK(cert.publicKey().sha256().hex() == "7b1394143f8d123760572cc410d1441b3bea3aaf987d0e4b950749e72d554db9");
+		}
+		
+		THEN("Selection should return the correct blob")
+		{
+			CHECK(cert.select(libdane::FullCertificate) == cert.encoded());
+			CHECK(cert.select(libdane::SubjectPublicKeyInfo) == cert.publicKey());
 		}
 	}
 	
 	GIVEN("An empty certificate")
 	{
 		Certificate cert;
+		
+		THEN("The certificate should be falsy")
+		{
+			CHECK(!cert);
+		}
 		
 		THEN("Issuer and Subject should be blank")
 		{
