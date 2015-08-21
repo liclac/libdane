47e6c0e0ae7abac1751dbd104ba4153d88e549cb
uppfinnarn <uppfinnarn@gmail.com>
2015-08-21 14:32:51 +0200

    More comments

--- include/libdane/common.h
+++ include/libdane/common.h
@@ -10,27 +10,26 @@ namespace libdane
 	 */
 	enum Usage {
 		/**
-		 * Only the given CA certificate or public key may be used to issue
-		 * certificates for this server, the CA is not implicitly trusted
-		 * if it's not in the system's trust store.
+		 * The root certificate is verified against the record.
+		 * 
+		 * It must also be trusted by the system's trust store to pass.
 		 */
 		CAConstraints = 0,
 		
 		/**
-		 * Only the specified certificate or public key may be used, it
-		 * must be valid and accepted by the system's trust store.
+		 * The topmost certificate is verified against the record.
+		 * 
+		 * It must also be trusted by the system's trust store to pass.
 		 */
 		ServiceCertificateConstraint = 1,
 		
 		/**
-		 * The given certificate or public key must be a trust anchor, and
-		 * is implicitly trusted regardless of the system's trust store.
+		 * Like CAConstraints, but the certificate is implicitly trusted.
 		 */
 		TrustAnchorAssertion = 2,
 		
 		/**
-		 * Like ServerCertificateConstraint, but the system's trust store
-		 * doesn't have to accept it.
+		 * Like ServerCertificateConstraint, but the certificate is implicitly trusted.
 		 */
 		DomainIssuedCertificate = 3,
 	};
@@ -41,8 +40,8 @@ namespace libdane
 	 * @see https://tools.ietf.org/html/rfc6698#section-2.1.2
 	 */
 	enum Selector {
-		FullCertificate = 0,		///< The binary certificate.
-		SubjectPublicKeyInfo = 1,	///< The DER-encoded public key.
+		FullCertificate = 0,		///< The DER-encoded certificate
+		SubjectPublicKeyInfo = 1,	///< The DER-encoded public key
 	};
 	
 	/**
