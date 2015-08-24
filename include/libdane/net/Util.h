fe8cfe85f16fb363c343c934610fa07731a4942e
uppfinnarn <uppfinnarn@gmail.com>
2015-08-24 17:18:14 +0200

    Documentation

--- include/libdane/net/Util.h
+++ include/libdane/net/Util.h
@@ -8,8 +8,14 @@ namespace libdane
 {
 	namespace net
 	{
+		/**
+		 * Parses a DANERecord from a resource record.
+		 */
 		DANERecord record_from_tlsa(ldns_rr *rr);
 		
+		/**
+		 * Overload that takes a Shared Pointer.
+		 */
 		inline DANERecord record_from_tlsa(std::shared_ptr<ldns_rr> rr) {
 			return record_from_tlsa(&*rr);
 		}
