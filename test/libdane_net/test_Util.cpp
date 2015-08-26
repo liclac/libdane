862430dfc900e0ed380d2cc5126fc2e507bc5f6f
uppfinnarn <uppfinnarn@gmail.com>
2015-08-26 18:13:37 +0200

    Unit tests for make_tlsa

--- test/libdane_net/test_Util.cpp
+++ test/libdane_net/test_Util.cpp
@@ -4,6 +4,52 @@
 using namespace libdane;
 using namespace libdane::net;
 
+SCENARIO("TLSA RRs can be created")
+{
+	GIVEN("A set of parameters")
+	{
+		std::shared_ptr<ldns_rr> rr = make_tlsa(CAConstraints, FullCertificate, SHA256Hash, Blob({ 0xFE, 0xEF }));
+		
+		THEN("The rdf count should be correct")
+		{
+			REQUIRE(ldns_rr_rd_count(&*rr) == 4);
+		}
+		
+		THEN("The usage should be correct")
+		{
+			ldns_rdf *usage_rd = ldns_rr_rdf(&*rr, 0);
+			REQUIRE(ldns_rdf_get_type(usage_rd) == LDNS_RDF_TYPE_INT8);
+			REQUIRE(ldns_rdf_size(usage_rd) == 1);
+			REQUIRE(ldns_rdf_data(usage_rd)[0] == CAConstraints);
+		}
+		
+		THEN("The selector should be correct")
+		{
+			ldns_rdf *selector_rd = ldns_rr_rdf(&*rr, 1);
+			REQUIRE(ldns_rdf_get_type(selector_rd) == LDNS_RDF_TYPE_INT8);
+			REQUIRE(ldns_rdf_size(selector_rd) == 1);
+			REQUIRE(ldns_rdf_data(selector_rd)[0] == FullCertificate);
+		}
+		
+		THEN("The matching should be correct")
+		{
+			ldns_rdf *matching_rd = ldns_rr_rdf(&*rr, 2);
+			REQUIRE(ldns_rdf_get_type(matching_rd) == LDNS_RDF_TYPE_INT8);
+			REQUIRE(ldns_rdf_size(matching_rd) == 1);
+			REQUIRE(ldns_rdf_data(matching_rd)[0] == SHA256Hash);
+		}
+		
+		THEN("The data should be correct")
+		{
+			ldns_rdf *data_rd = ldns_rr_rdf(&*rr, 3);
+			REQUIRE(ldns_rdf_get_type(data_rd) == LDNS_RDF_TYPE_HEX);
+			REQUIRE(ldns_rdf_size(data_rd) == 2);
+			REQUIRE(ldns_rdf_data(data_rd)[0] == 0xFE);
+			REQUIRE(ldns_rdf_data(data_rd)[1] == 0xEF);
+		}
+	}
+}
+
 SCENARIO("Records can be parsed from TLSA RRs")
 {
 	GIVEN("Different Usages")
